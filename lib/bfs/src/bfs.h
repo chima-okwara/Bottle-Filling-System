39e2103eabdf6fbb48dd9fbf6426277de3975b4f
chima-okwara <cokwara82@gmail.com>
2023-01-10 21:51:38 +0100

    Streamlined development model. Optimised code.

--- lib/bfs/src/bfs.h
+++ lib/bfs/src/bfs.h
@@ -1,23 +1,26 @@
 #ifndef __BFS__
 #define __BFS__
 #include <LiquidCrystal.h>
+#include <Wire.h>
 
-#define MAX   50
-#define STEPS 2000000
-#define DELAY 30
+#define MAX   6
+#define STEPS 900
+#define __DELAY__ 300
+#define front HIGH
+#define back  LOW
 
 
 //Connections:
 extern LiquidCrystal lcd;
-extern uint8_t dirPin, stepperPin, pump, sw1, sw2, sw3, buzzer;
+//extern uint8_t dirPin, stepperPin, pump, sw1, sw2, sw3, buzzer;
+extern uint8_t sw1, sw2, sw3;
+extern uint8_t pump, buzzer, dirPin, stepperPin;
 
 //Variables:
 extern uint8_t bottleNo;
 extern bool start;
 
 //Functions:
-void setup(void);
-void loop(void);
 void step(const uint8_t&, const int&);
 void fillBottles(uint8_t &num);               //To fill bottles
 void getNoBottles(void);
